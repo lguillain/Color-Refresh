77620877a3719e9651174c29bf548de743830391
Leonore <leonore.guillain@bluewin.ch>
2025-04-11 10:44:20 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import './App.css'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen bg-gray-50">
       <div className="container mx-auto">
-        <h1 className="text-2xl font-bold mb-6 text-center">Taskbase Color Palette Tester</h1>
         <TaskbaseMockup />
       </div>
     </div>
